005b95d148ba72be673554759e05347cc9670e17
Orhan Burak Bozan <OrhanBurak.Bozan@n11.com>
2021-02-05 15:51:13 +0300

    klunge-3 (#3) unit tests added

--- java-api/src/main/java/com/kloia/eventapis/api/impl/UUIDCreationStrategy.java
+++ java-api/src/main/java/com/kloia/eventapis/api/impl/UUIDCreationStrategy.java
@@ -4,12 +4,11 @@ import com.kloia.eventapis.api.IdCreationStrategy;
 
 import java.util.UUID;
 
-/**
- * Created by zeldal on 25/05/2017.
- */
 public class UUIDCreationStrategy implements IdCreationStrategy {
+
     @Override
     public String nextId() {
         return UUID.randomUUID().toString();
     }
+
 }
